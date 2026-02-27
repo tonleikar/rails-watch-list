6e4ccb497087319ef300e4c6a21a3dc7f0e145c9
Sam Gorman-Hailey <sam.gormanhailey@pm.me>
2026-02-27 16:45:49 +0000

    add img upload

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,6 @@ class List < ApplicationRecord
   validates :name, presence: true
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+
+  has_one_attached :photo
 end
